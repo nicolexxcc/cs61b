229bcf23e49d00938e0cd05089cb2021154631b2
nicolexxcc <nicolex.li@hotmail.com>
2020-03-13 13:26:34 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		while (i < 10) {
+		sum = sum + i;
+		i = i + 1;
+	}
+	System.out.println(sum);
+	
+	}
+}
