fc3540c86b70de4e40369d4a547e17be2612ce18
nicolexxcc <nicolex.li@hotmail.com>
2020-07-26 22:59:30 +0800

    lab9 completed

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -7,7 +7,7 @@ import java.util.Set;
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ *  @author Ruoxuan Li
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -53,19 +53,27 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        int number = hash(key);
+        return buckets[number].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        int number = hash(key);
+        if (!buckets[number].containsKey(key)) {
+            size += 1;
+            buckets[number].put(key, value);
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
